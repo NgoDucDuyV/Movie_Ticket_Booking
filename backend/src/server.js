7c3373bae84cd506075184a3eacdae93eee872b5
Ngo Duc Duy <nducduyfpoly.vn@gmail.com>
2026-01-22 12:33:19 +0700

    update "dev": "nodemon --watch src src/server.js", update import router

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,10 @@ import {
   errorHandler,
   notFoundHandler,
 } from "./middleware/error.middleware.js";
-
+import router from "./routes/index.js";
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
 // ============================================
@@ -43,6 +43,8 @@ app.get("/", (req, res) => {
     status: "running",
   });
 });
+
+app.use("/api", router);
 
 // API routes
 // import router from "./routes/index.js";
